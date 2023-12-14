d53f151e4f3426b77aae0671fa3ca9aa0635e635
Thamos4 <tomassemapelido420@gmail.com>
2023-12-15 00:23:59 +0100

    GG - Added Email and password inputs to Login view

--- ProjetoP3/Core/Authentication/LoginView.swift
+++ ProjetoP3/Core/Authentication/LoginView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationStack {
             VStack{
                 // image (dps arranjamos um logo)
                 
                 // form fields
+                VStack(spacing: 24){
+                    InputView(text: $email, title: "Email", placeholder: "email@example.com")
+                        .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                            isSecureField: true)
+                        .autocapitalization(.none)
+                    
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 
                 // sign in button
+                Spacer()
                 
                 // sign up button
             }

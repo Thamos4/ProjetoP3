81b16dd505f45eedb5f035299db01ba1443d749d
Thamos4 <tomassemapelido420@gmail.com>
2023-12-14 17:35:41 +0100

    Initial Commit

---
+++ ProjetoP3/ProjetoP3App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoP3App.swift
+//  ProjetoP3
+//
+//  Created by user243107 on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoP3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

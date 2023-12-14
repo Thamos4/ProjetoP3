74b1f704a5f8cf7fb108e3b28026378747c330de
Thamos4 <tomassemapelido420@gmail.com>
2023-12-15 00:03:13 +0100

    GG - Created base folder structure

---
+++ ProjetoP3/Core/Authentication/LoginView.swift
@@ -0,0 +1,30 @@
+//
+//  LoginView.swift
+//  ProjetoP3
+//
+//  Created by user243107 on 12/14/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack{
+                // image (dps arranjamos um logo)
+                
+                // form fields
+                
+                // sign in button
+                
+                // sign up button
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

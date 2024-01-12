dd8d23691eefc92da2d4e6b1e9a80c3e33a64f2b
Thamos4 <tomassemapelido420@gmail.com>
2024-01-12 23:40:41 +0100

    SR - Modified article view to add comments

--- ProjetoP3/Core/Articles/ArticleView.swift
+++ ProjetoP3/Core/Articles/ArticleView.swift
@@ -8,57 +8,98 @@
 import SwiftUI
 
 struct ArticleView: View {
-    @EnvironmentObject var viewModel: AuthViewModel
+    @EnvironmentObject var userViewModel: AuthViewModel
     @StateObject var articleViewModel = ArticleViewModel()
+    @StateObject var commentViewModel = CommentViewModel()
+    @State private var currentCommentList: [articleComment] = []
+    @State private var newCommentContent: String = ""
     let article: Article
    
     var body: some View {
-        VStack {
-            HStack{
-                NavigationLink(destination: Home()
-                    .navigationBarBackButtonHidden(true)) {
-                    Image(systemName: "arrow.left")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    }.foregroundColor(.black)
-                
-//                Image(systemName: "magnifyingglass")
-               
-                
-            }.padding(.horizontal)
-            .padding(.bottom, 12)
-            .font(.system(size: 16))
-            
-            VStack{
-                HStack {
-                    Text(article.title)
-                        .font(.title)
-                        .padding(.top, 5)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+        NavigationStack{
+            VStack {
+                HStack{
+                    NavigationLink(destination: Home()
+                        .navigationBarBackButtonHidden(true)) {
+                        Image(systemName: "arrow.left")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        }.foregroundColor(.black)
+                    
+    //                Image(systemName: "magnifyingglass")
+                   
+                    
                 }.padding(.horizontal)
-                    .padding(.top, 12)
-                HStack {
-                    Image(systemName: "person.fill")
-                        .frame(width: 20, height: 20)
-                    Text(article.author)
-                        .font(.system(size: 14))
-                }.padding(.horizontal)
-                HStack {
-                    Image(systemName: "door.right.hand.closed")
-                        .frame(width: 20, height: 20)
-                    Text(article.room)
-                        .font(.system(size: 14))
-                }
-                HStack {
-                    Image(systemName: "doc.text.fill")
-                        .frame(width: 20, height: 20)
-                    Text(article.summary)
-                        .font(.system(size: 14))
+                .padding(.bottom, 12)
+                .font(.system(size: 16))
+                
+                VStack{
+                    HStack {
+                        Text(article.title)
+                            .font(.title)
+                            .padding(.top, 5)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding(.horizontal)
+                        .padding(.top, 12)
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .frame(width: 20, height: 20)
+                        Text(article.author)
+                            .font(.system(size: 14))
+                    }.padding(.horizontal)
+                    HStack {
+                        Image(systemName: "door.right.hand.closed")
+                            .frame(width: 20, height: 20)
+                        Text(article.room)
+                            .font(.system(size: 14))
+                    }
+                    HStack {
+                        Image(systemName: "doc.text.fill")
+                            .frame(width: 20, height: 20)
+                        Text(article.summary)
+                            .font(.system(size: 14))
+                    }
+                    Spacer()
+                    HStack{
+                        InputView(imageName: "pencil", placeholder: "Write a question here", text: $newCommentContent)
+                        Button{
+                            Task {
+                                try await commentViewModel.addComment(articleId: article.id, userId: userViewModel.currentUser!.id,content: newCommentContent)
+                                newCommentContent = ""
+                                print("DEBUG: Pressed add comment button")
+                            }
+                        }label:{ Image(systemName: "paperplane")
+                                .font(.system(size: 20))
+                                .foregroundColor(.white)
+                                .padding(12)
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        .disabled(!formIsValid)
+                        .opacity(!formIsValid ? 0.5 : 1.0)
+                    }
+                    
+                }.onAppear{
+                    Task {
+                        try await commentViewModel.getCommentsByArticleId(articleId:article.id)
+                        currentCommentList = commentViewModel.comments
+                    }
                 }
             }
-            Spacer()
+            .padding(.top, 12)
+            .padding(.horizontal)
+            
+            
+            
+            ForEach(currentCommentList){ comment in
+                CommentContainerView(comment: comment).environmentObject(userViewModel)
+            }
         }
-        .padding(.top, 12)
-        .padding(.horizontal)
+    }
+}
+
+extension ArticleView: AuthenticationFormProtocol{
+    var formIsValid: Bool{
+        return newCommentContent.count > 5
     }
 }
 

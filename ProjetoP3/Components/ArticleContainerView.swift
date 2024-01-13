e29abcec78be83936069dcad4d072534e5702eba
Thamos4 <tomassemapelido420@gmail.com>
2024-01-13 15:40:41 +0000

    GG - Updated ArticleContainerView

--- ProjetoP3/Components/ArticleContainerView.swift
+++ ProjetoP3/Components/ArticleContainerView.swift
@@ -10,69 +10,110 @@ import SwiftUI
 struct ArticleContainerView: View {
     @EnvironmentObject var viewModel: AuthViewModel
     @StateObject var articleViewModel = ArticleViewModel()
+    
     let article: Article
     @State private var showAlert = false
     var body: some View {
-        VStack(alignment: .leading, spacing: 15){
-            HStack {
-                Text(article.title)
-                    .font(.title3)
-                    .foregroundColor(Color(.white))
-                    .bold()
-                
-                Text(article.author)
-                    .font(.headline)
-                    .foregroundColor(Color(.white))
-                    .bold()
-                
-                Text(article.summary)
-                    .font(.headline)
-                    .foregroundColor(Color(.white))
-                    .bold()
-                HStack{
+        if let user = viewModel.currentUser {
+        VStack(alignment: .leading){
+                VStack {
+                    HStack {
+                        Image(systemName: "newspaper.fill")                .frame(width: 20, height: 20)
+                            .foregroundColor(.white)
+                        Text("\(article.title)")
+                            .font(.headline)
+                            .foregroundColor(Color(.white))
+                            .bold()
+                        
+                        Spacer()
+            
+                        
+                        HStack {
+                            Image(systemName: "clock")                .frame(width: 20, height: 20)
+                                .foregroundColor(.white)
+                            Text("8:00")
+                                .foregroundColor(Color(.white))
+                        }
+
+                    }.padding(.vertical, 6)
                     
-                    if let user = viewModel.currentUser, user.role == .admin {
-                        Button {
-                            print("Lol123")
-                        } label: {
-                            Image(systemName: "pencil")
-                                .foregroundColor(Color(.red))
-                                .font(.system(size: 13))
-                        }
+                    VStack(alignment: .leading, spacing: 12){
                         
-                        Button {
-                            self.showAlert = true
-                        } label: {
-                            Image(systemName: "trash")
-                                .foregroundColor(Color(.red))
-                                .font(.system(size: 13))
-                        }.alert(isPresented: $showAlert) {
-                            Alert(title: Text("Delete Article?"),
-                                  message: Text("Do you really want to delete this article? "),
-                                  primaryButton: .default(Text("Yes"), action:{
-                                self.showAlert = false
-                                
-                                Task {
-                                    try await articleViewModel.deleteArticle(id: article.id)
-                                }
-                            }),
-                                  secondaryButton: .default(Text("No"), action:{
-                                
-                                self.showAlert = false
-                            }))
-                        }
+                        Text(article.summary)
+                            .font(.system(size: 15))
+                            .foregroundColor(Color(.white))
                         
+                        Text("Track: Informática")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color(.white))
+                        
+                        HStack {
+                            HStack {
+                                Image(systemName: "person.fill")               .font(.system(size: 15))
+                                    .foregroundStyle(.secondary)
+                                    .foregroundColor(.white)
+                                Text(article.author)
+                                    .font(.system(size: 14))
+                                    .foregroundStyle(.secondary)
+                                    .foregroundColor(Color(.white))
+                            }
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Image(systemName: "door.left.hand.closed")               .font(.system(size: 15))
+                                    .foregroundStyle(.secondary)
+                                    .foregroundColor(.white)
+                                Text(article.room)
+                                    .font(.system(size: 14))
+                                    .foregroundStyle(.secondary)
+                                    .foregroundColor(Color(.white))
+                            }
+                        }.padding(.top, 6)
+       
+                    }.frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 6)
+
+                }
+            
+            if user.role == .admin {
+                HStack{
+                    Image(systemName: "pencil")
+                        .foregroundColor(Color(.white))
+                        .font(.system(size: 13))
+                    
+                    Button {
+                        self.showAlert = true
+                    } label: {
+                        Image(systemName: "trash")
+                            .foregroundColor(Color(.red))
+                            .font(.system(size: 13))
+                    }.alert(isPresented: $showAlert) {
+                        Alert(title: Text("Delete Article?"),
+                              message: Text("Do you really want to delete this article? "),
+                              primaryButton: .default(Text("Yes"), action:{
+                            self.showAlert = false
+                            
+                            Task {
+                                try await articleViewModel.deleteArticle(id: article.id)
+                            }
+                        }),
+                              secondaryButton: .default(Text("No"), action:{
+                            
+                            self.showAlert = false
+                        }))
                     }
                 }
+                .padding(.top, 1)
             }
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(Color("TaskBG"))
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-            
-            
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color("TaskBG"))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding()
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+        
         }
     }
 }
